8f7f077ddc50a5fb20dbe10e882682654f5397cb
Rafał Gwiozda <rafal.gwiozda@gmail.com>
2019-11-14 19:09:38 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,23 @@
+console.log('siema🤣🤣🤣🤣👌👌👌👌');
+const name='Rafał Gwiozda';
+const age='35';
+console.log(name);
+console.log(age);
+console.log(`nNazywam się ${name} i mam ${age} lat`);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
